3764a72e3c18d8b375921d41479c173abc5dee8e
Simon Støvring <simonstoevring@gmail.com>
2016-02-22 21:28:15 +0100

    Adds content on not found page

--- app/components/MapError.js
+++ app/components/MapError.js
@@ -9,15 +9,17 @@ var MapError = React.createClass({
     return (
       <div>
       <div className='map-overlay'></div>
-        <div className='map-overlay-content'>
-          <div className='map-error-title'>😭</div>
-          <div className='map-error-message'>Authentication with Jodel failed. Please ensure that you have given Jodelihood access to your location as this is required for authenticating with Jodel.</div>
-          <div className='map-error-retry'>
-            <a href='javascript:window.location.href=window.location.href'>
-              Let's try that again 🚀
-            </a>
-          </div>
-        </div>
+      <div className='map-overlay-content'>
+      <div className='error'>
+      <div className='error-title'>😭</div>
+      <div className='error-message'>Authentication with Jodel failed. Please ensure that you have given Jodelihood access to your location as this is required for authenticating with Jodel.</div>
+      <div className='map-error-retry'>
+      <a href='javascript:window.location.href=window.location.href'>
+      Let's try that again 🚀
+      </a>
+      </div>
+      </div>
+      </div>
       </div>
     )
   }
--- app/components/NotFoundPage.js
+++ app/components/NotFoundPage.js
@@ -4,7 +4,10 @@ var NotFoundPage = React.createClass({
   render: function() {
     return (
       <div className='page-content'>
-      <p>Not found</p>
+      <div className='error'>
+      <div className='error-title'>👀</div>
+      <div className='error-message'>The page you were looking for could not be found.</div>
+      </div>
       </div>
     )
   }
