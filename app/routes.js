4e81d3ef382e5b33d59b84ca847348612d69d04e
Simon Støvring <simonstoevring@gmail.com>
2016-02-22 08:39:41 +0100

    Clean up

--- app/routes.js
+++ app/routes.js
@@ -3,7 +3,6 @@ var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var IndexRoute = ReactRouter.IndexRoute;
-var NotFoundRoute = ReactRouter.NotFoundRoute;
 
 var Main = require('./components/Main');
 var MapPage = require('./components/MapPage');
